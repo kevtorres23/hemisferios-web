39fbd908e955ac2de020fc8b82ddeb93a3c7945d
Kevin Torres <kevintu236@gmail.com>
2026-01-27 20:07:59 -0600

    Finished the MIssion and Vission sections.

--- frontend/app/about-us/page.tsx
+++ frontend/app/about-us/page.tsx
@@ -1,6 +1,9 @@
 import Navbar from "@/website-components/Navbar"
 import Footer from "@/website-components/Footer"
 import InitialSection from "@/website-sections/about-us/InitialSection";
+import MissionAndVission from "@/website-sections/about-us/MissionAndVission";
+import missionImage from "../../public/about-us-2.svg";
+import vissionImage from "../../public/about-us-3.svg";
 
 function AboutUs() {
     return (
@@ -9,6 +12,20 @@ function AboutUs() {
 
             <div className="relative overflow-x-hidden overflow-y-hidden flex flex-col min-h-screen items-center justify-center bg-white font-sans dark:bg-black">
                 <InitialSection />
+
+                <MissionAndVission
+                    type="mission"
+                    title="Ofrecer un servicio terapéutico de calidad para todos y todas."
+                    description="Hemisferios es un centro de terapia creado con el propósito de poder ayudar a niñas y niños del estado de Durango."
+                    image={missionImage}
+                />
+
+                <MissionAndVission
+                    type="vission"
+                    title="Llegar a ser un espacio líder en la ciudad en el área del desarrollo cognitivo."
+                    description="Hemisferios es un centro de terapia creado con el propósito de poder ayudar a niñas y niños del estado de Durango."
+                    image={vissionImage}
+                />
 
                 <Footer />
             </div>
