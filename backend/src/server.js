25047e9af77546789b64622cc702b3325f91a40e
Kevin Torres <kevintu236@gmail.com>
2026-02-10 15:31:53 -0600

    Added a simple rate limit middleware.

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,26 @@ import cors from "cors";
 import colors from "colors";
 import appointmentRoutes from "./routes/appointmentRoutes.js";
 import { connectDB } from "../config/db.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-// Calling the connection method.
-connectDB();
+// Middlewares: functions that run in the middle of a response and a request. They are executed before sending the response back.
+app.use(express.json()); // Parse JSON bodies and allow access to req.body
+app.use(rateLimiter);
 
-// Middleware
-app.use(express.json());
 
 app.use("/api/appointments", appointmentRoutes);
 // We'll add here the rest of the routes, such as contact messages, clients, therapists, etc...
 
 app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server started on PORT: 5001`.green)
+// Connecting to the DB first, and then connecting to the port.
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server started on PORT: 5001`.green)
+    });
 });
