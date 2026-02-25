78c59cb41f95d8e501f9e171fc6c9fdbe808a762
Kevin Torres <kevintu236@gmail.com>
2026-02-25 15:38:25 -0600

    Adjusted the weekCreator function and implemented it in the calendar's intervals.

--- frontend/components/system/appointments/AppointmentCalendar.tsx
+++ frontend/components/system/appointments/AppointmentCalendar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { weekCreator } from "@/utils/system/calendar-methods";
+import { weekCreator, nextWeekNum, daysDistance } from "@/utils/system/calendar-methods";
 import { useState } from "react";
 import PageNavigator from "../PageNavigator";
 import { AppointmentType } from "@/utils/types";
@@ -12,7 +12,51 @@ type CalendarProps = {
     page: "history" | "appointments";
 };
 
-console.log(weekCreator(30, 5));
+type IntervalValue = {
+    day: number | undefined,
+    month: string,
+};
+
+type WeekInterval = {
+    firstValue: IntervalValue,
+    secondValue: IntervalValue,
+};
+
+const months = ["ene.", "feb.", "mar.", "abr.", "mayo", "jun.", "jul.", "ago.", "sep.", "oct.", "nov.", "dic."]
+
+// Date variables.
+const date = new Date();
+const currentMonthNum = date.getMonth();
+const todayMonthNum = date.getDate();
+const nextWeekNumber = nextWeekNum(todayMonthNum);
+
+// Week lists for the calendar.
+const currentWeekList = weekCreator(todayMonthNum, currentMonthNum);
+const nextWeekList = weekCreator(nextWeekNumber.day, nextWeekNumber.month);
+
+// Interval values for the calendar.
+const currentInterval: WeekInterval = {
+    firstValue: {
+        day: currentWeekList[0].dayNum.number, // This key is equal to the day-of-the-month of the week's monday (start of the interval).
+        month: months[currentWeekList[0].dayNum.month], // The month's name abbreviated, indexed from the months's array.
+    },
+    secondValue: {
+        day: currentWeekList[5].dayNum.number, // This key is equal to the day-of-the-month of the week's saturday (end of the interval).
+        month: months[currentWeekList[5].dayNum.month], 
+    }
+};
+
+const nextInterval: WeekInterval = {
+    firstValue: {
+        day: nextWeekList[0].dayNum.number,
+        month: months[nextWeekList[0].dayNum.month],
+    },
+    secondValue: {
+        day: nextWeekList[5].dayNum.number,
+        month: months[nextWeekList[5].dayNum.month],
+    }
+};
+
 
 function AppointmentCalendar(props: CalendarProps) {
     // State variables.
@@ -27,7 +71,7 @@ function AppointmentCalendar(props: CalendarProps) {
                         <PageNavigator
                             onPreviousClick={() => setWeek(1)}
                             onNextClick={() => setWeek(2)}
-                            labelText="Semana actual"
+                            labelText={week === 1 ? "Semana actual" : "Semana siguiente"}
                             currentPage={week}
                             finalPage={numberOfWeeks}
                             labelStyles="text-2xl font-medium text-slate-900 tracking-tight text-center"
@@ -46,7 +90,17 @@ function AppointmentCalendar(props: CalendarProps) {
                     )}
 
                     <div className="week-indicator px-2.5 py-1 border border-indigo-400 bg-indigo-50 rounded-sm">
-                        <p className="text-sm text-indigo-500 font-medium">09/02 - 14/02</p>
+                        <p className="text-sm text-indigo-500 font-medium">
+                            {week === 1 ? (
+                                // Build the current or next week interval, like: "23 de feb. - 28 de feb."
+                                currentInterval.firstValue.day + " de " + currentInterval.firstValue.month + " - " +
+                                currentInterval.secondValue.day + " de " + currentInterval.secondValue.month
+                            ) : (
+                                nextInterval.firstValue.day + " de " + nextInterval.firstValue.month + " - " +
+                                nextInterval.secondValue.day + " de " + nextInterval.secondValue.month
+                            )
+                            }
+                        </p>
                     </div>
                 </div>
 
@@ -56,6 +110,6 @@ function AppointmentCalendar(props: CalendarProps) {
             <CalendarUI data={props.data} page={props.page} />
         </div>
     );
-}
+};
 
 export default AppointmentCalendar;
--- frontend/utils/system/calendar-methods.tsx
+++ frontend/utils/system/calendar-methods.tsx
@@ -1,4 +1,33 @@
-// PURPOSE OF THE MODULE: to perform calculations needed for the calendar or the availability.
+// PURPOSE OF THE MODULE: to perform calculations needed for the calendar and the appointment availability.
+
+// Global scope variables.
+const date = new Date();
+const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+const currentMonth = date.getMonth();
+
+// Global scope arrow functions.
+const getDaysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate(); // Calculate the number of days in a given month.
+
+// Calculates the day-of-the-month of today's day from the next week. Ex. Today: 25 (Wednesday, Feb), Next week: 4 (Wednesday, March).
+function nextWeekNum(todayMonthNum: number) {
+    const daysInCurrentMonth = getDaysInMonth(2026, currentMonth + 1);
+    const nextWeekNum = todayMonthNum + 7;
+    let nextMondayObj;
+
+    if (nextWeekNum > daysInCurrentMonth) {
+        nextMondayObj = {
+            day: nextWeekNum - daysInCurrentMonth, // The difference of days, example: 30 - 28 = 2
+            month: currentMonth + 1,
+        };
+    } else {
+        nextMondayObj = {
+            day: nextWeekNum,
+            month: currentMonth,
+        };
+    };
+
+    return nextMondayObj;
+};
 
 import { AppointmentType } from "@/utils/types";
 
@@ -17,36 +46,23 @@ function calendarHoursCreator(appointments: AppointmentType[]) {
     return calendarHours;
 };
 
-// This function returns the difference of days between today and another day (second parameter).
+// This function returns the difference of days between today and another day, positive por past days, negative for future ones.
 function daysDistance(todaysNum: number, comparativeDayNum: number) {
     const days = [1, 2, 3, 4, 5, 6]; // This array represents the days with their corresponding number, where Monday is 1.
 
-    return (days.indexOf(todaysNum) - days.indexOf(comparativeDayNum)); // It returns the difference of days between today and another day (second argument).
+    return (days.indexOf(todaysNum) - days.indexOf(comparativeDayNum)); // Return the difference of days between today and another day (second argument).
 };
 
-// Soluciones para el calendario: Crear un objeto con toda la info.
-
-// This function pairs the week's day names with their corresponding month number, from the current week and from the next one, like "Monday 10".
-function weekCreator(todayMonthNum: number, currentMonthNum: number) {
-    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-    const currentWeek = []; // Array to save the pairs of the current week's day names and month numbers.
-    const nextWeek = []; // Array to save the pairs of the next week's day names and month numbers.
-
-    const date = new Date();
-
-    const currentMonth = currentMonthNum;
-
-    const getDaysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate(); // Calculate the number of days in a given month.
-    const daysInPreviousMonth = getDaysInMonth(2026, currentMonth);
-    const daysInCurrentMonth = getDaysInMonth(2026, currentMonth - 1);
-    console.log("Días", daysInCurrentMonth);
-    const daysInNextMonth = getDaysInMonth(2026, currentMonth + 2);
-
+// Pairs the week's day names with their corresponding month number.
+function weekCreator(todayMonthNum: number, monthNum: number) {
+    const createdWeek = []; // Array to save the pairs of the current week's day names and month numbers.
     const todayWeekNum = date.getDay(); // This is equal to today's day in number, for example, 4 (for Jueves).
-    // getDate to obtain the month number.
+    const daysInCurrentMonth = getDaysInMonth(2026, monthNum + 1);
+    const daysInPreviousMonth = getDaysInMonth(2026, monthNum);
 
+    const month = monthNum;
+
+    // Loop variables.
     let day;
     let num;
     let newMonthCounter = 1;
@@ -59,10 +75,13 @@ function weekCreator(todayMonthNum: number, currentMonthNum: number) {
         num = todayMonthNum - distance;
 
         if ((num <= daysInCurrentMonth) && (num >= 1)) {
-            currentWeek.push(
+            createdWeek.push(
                 {
                     dayName: day,
-                    dayNum: num,
+                    dayNum: {
+                        number: num,
+                        month: month,
+                    },
                 }
             );
 
@@ -70,19 +89,25 @@ function weekCreator(todayMonthNum: number, currentMonthNum: number) {
         };
 
         if (num < 1) {
-            currentWeek.push(
+            createdWeek.push(
                 {
                     dayName: day,
-                    dayNum: previousMonthCounter - (distance - 1),
+                    dayNum: {
+                        number: previousMonthCounter - (distance - 1),
+                        month: month - 1,
+                    },
                 },
             );
         };
 
         if (num > daysInCurrentMonth) {
-            currentWeek.push(
+            createdWeek.push(
                 {
                     dayName: day,
-                    dayNum: newMonthCounter,
+                    dayNum: {
+                        newMonthCounter,
+                        month: month + 1,
+                    },
                 },
             );
 
@@ -90,7 +115,7 @@ function weekCreator(todayMonthNum: number, currentMonthNum: number) {
         }
     };
 
-    return (currentWeek);
+    return (createdWeek);
 };
 
-export { calendarHoursCreator, daysDistance, weekCreator };
+export { nextWeekNum, calendarHoursCreator, daysDistance, weekCreator };
