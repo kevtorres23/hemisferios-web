eadb30f85f9ea1b5b5e4a23319958b541b847112
Kevin Torres <kevintu236@gmail.com>
2026-03-11 15:54:16 -0600

    Rewrote the next-payment calculation functions by implementing date-fns submodules (addDays, addMonths, and compareAsc.

--- frontend/utils/system/patients/next-payments.ts
+++ frontend/utils/system/patients/next-payments.ts
@@ -1,6 +1,5 @@
 import { lessThanTen } from "@/utils/format-availability";
-import { getDaysInMonth } from "../calendar/calendar-methods";
-import { compareAsc } from "date-fns";
+import { addDays, addMonths, compareAsc } from "date-fns";
 
 /**
  * Calculates the next payment's date for a patient right after its starting date in the center.
@@ -14,44 +13,18 @@ function calculateNextPayment(frequency: string, startingDate: string) {
     const month = startingDate[3] + startingDate[4];
     const year = startingDate[6] + startingDate[7] + startingDate[8] + startingDate[9];
 
-    const daysInMonth = getDaysInMonth(Number(year), Number(month));
+    const builtDate = new Date(Number(year), Number(month), Number(day));
+    let newDate;
     let nextPaymentDate;
 
     if (frequency === "weekly") {
         // Date variables for the 'weekly' frequency type.
-        let newDay = Number(day) + 7;
-        let newMonth = Number(month);
-        let newYear = Number(year);
-
-        if (newDay > daysInMonth) {
-            newDay -= daysInMonth; // The difference of the days that exceeded the month's limit.
-            newMonth += 1;
-        };
-
-        // Handle exceeding the month indexes (a new year).
-        if (newMonth > 12) {
-            newMonth = 1;
-            newYear += 1;
-        };
-
-        nextPaymentDate = lessThanTen(newDay) + "/" + lessThanTen(newMonth) + "/" + lessThanTen(newYear);
-
+        newDate = addDays(builtDate, 7);
     } else {
-        // Date variables for the 'monthly' frequency type.
-        let newMonth = Number(month);
-        let newYear = Number(year);
-
-        newMonth += 1;
-
-        // Handle exceeding the month indexes (a new year).
-        if (newMonth > 12) {
-            newMonth = 1;
-            newYear += 1;
-        };
-
-        nextPaymentDate = lessThanTen(Number(day)) + "/" + lessThanTen(newMonth) + "/" + lessThanTen(newYear);
+        newDate = addMonths(builtDate, 1);
     };
 
+    nextPaymentDate = lessThanTen(newDate.getDate()) + "/" + lessThanTen(newDate.getMonth()) + "/" + lessThanTen(newDate.getFullYear());
     return nextPaymentDate;
 };
 
@@ -68,23 +41,10 @@ function establishPaymentDate(frequency: string, date: string) {
     // Calcuate the nearest payment date from the date parameter.
     let nextPaymentDate = calculateNextPayment(frequency, date);
 
-    switch (frequency) {
-        case "weekly":
-            // Condition 1: The next payment day must be greater or equal than today.
-            // Condition 2: The next payment month must be greater or equal than the current month.
-            if (compareAsc(currentDate, date) != 1) {
-                return nextPaymentDate;
-            } else {
-                establishPaymentDate(frequency, nextPaymentDate); // If the condition is not met, we recall the function with the newly calculated date, until the conditions are met.
-            };
-        case "monthly":
-            // Condition 1: The next payment day must be  equal than the day of the date passed as argument.
-            // Condition 2: The next payment month must be greater than the current month.
-            if (compareAsc(currentDate, date) != 1) {
-                return nextPaymentDate;
-            } else {
-                establishPaymentDate(frequency, nextPaymentDate); // If the condition is not met, we recall the function with the newly calculated date, until the conditions are met.
-            };
+    if (compareAsc(currentDate, date) != 1) {
+        return nextPaymentDate;
+    } else {
+        establishPaymentDate(frequency, nextPaymentDate); // If the condition is not met, we recall the function with the newly calculated date, until the conditions are met.
     };
 };
 
